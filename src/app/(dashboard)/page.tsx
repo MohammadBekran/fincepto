0bd48a2b94d621d5e22836ba8499809022d276b5
Mohammad <bhshtbaran@gmail.com>
2024-12-20 12:03:31 -0800

    feat: add force dynamic to the dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import Dashboard from "@/features/dashboard/components";
 
+export const dynamic = "force-dynamic";
+
 const DashboardPage = () => {
   return <Dashboard />;
 };
