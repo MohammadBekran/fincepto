249ddf40dd4ffb19713b407dba14c8f198a94117
Mohammad <bhshtbaran@gmail.com>
2024-12-20 12:08:30 -0800

    feat: add force dynamic to the settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import Settings from "@/features/settings/components";
 
+export const dynamic = "force-dynamic";
+
 const SettingsPage = () => {
   return <Settings />;
 };
