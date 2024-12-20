6f2d7fada45f641bac864df7f71c976fc3ad7d57
Mohammad <bhshtbaran@gmail.com>
2024-12-20 11:12:15 -0800

    fix: fix a bug

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 import Accounts from "@/features/accounts/components";
 
 const AccountsPage = () => {
-  return <Accounts />;
+  return (
+    <Suspense>
+      <Accounts />;
+    </Suspense>
+  );
 };
 
 export const metadata: Metadata = {
